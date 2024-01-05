27cd4130511dc83844fffd64b355510b7ecc5617
Thiago <thiagoleonardo1995@live.com>
2024-01-05 14:55:42 -0300

    Employee: Controller, Service, Test findAllPagedSortByName Ok

---
+++ src/main/java/com/devsuperior/demo/services/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.EmployeeDTO;
+import com.devsuperior.demo.entities.Department;
+import com.devsuperior.demo.entities.Employee;
+import com.devsuperior.demo.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired // Acesso ao Banco de Dados
+	private EmployeeRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<EmployeeDTO> findAll(Pageable pageable) {
+		Page<Employee> page = repository.findAll(pageable);
+		// converter lista para DTO e devolver
+		return page.map(x -> new EmployeeDTO(x));
+	}
+}
